045231bee1e4d7fe0ec044d9dc31e50767011cbc
guneyeroglu <benguneyeroglu@gmail.com>
2024-09-01 02:08:56 +0300

    update: config

--- config/config.go
+++ config/config.go
@@ -1,19 +1,16 @@
 package config
 
 import (
-	"log"
-
 	"github.com/spf13/viper"
 )
 
 func Init() {
 	viper.SetConfigFile(".env")
 	viper.AddConfigPath(".")
-	viper.AutomaticEnv()
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		log.Fatalf("Error reading .env file: %s", err)
+		viper.AutomaticEnv()
 	}
 
 }

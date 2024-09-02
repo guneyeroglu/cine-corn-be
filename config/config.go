a8649ca6df113d4db66a06fb377798e134d2ceea
guneyeroglu <benguneyeroglu@gmail.com>
2024-09-03 01:09:26 +0300

    update: build problem fixed -fn

--- config/config.go
+++ config/config.go
@@ -9,14 +9,10 @@ import (
 func Init() {
 	viper.SetConfigFile(".env")
 	viper.AddConfigPath(".")
-	viper.AutomaticEnv()
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Println("No .env file found, using environment variables instead")
-		} else {
-			log.Fatalf("Error reading .env file: %s", err)
-		}
+		log.Println("No .env file found, using environment variables instead")
+		viper.AutomaticEnv()
 	}
 }
